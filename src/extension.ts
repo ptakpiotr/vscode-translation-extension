185884a76ed4b72262588288a70fddc342b2e694
ptakpiotr <piter020@outlook.com>
2025-07-31 22:33:32 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+import * as vscode from "vscode";
+
+function findTrail(
+  symbols: vscode.DocumentSymbol[],
+  pos: vscode.Position
+): string[] {
+  //find trail
+  for (const symbol of symbols) {
+    if (symbol.range.contains(pos)) {
+      const childTrail = findTrail(symbol.children, pos);
+      return [symbol.name, ...childTrail];
+    }
+  }
+  return [];
+}
+
+async function getSymbolTrail(): Promise<string> {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) return "";
+
+  const position = editor.selection.active;
+
+  //get all symbols
+  const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+    "vscode.executeDocumentSymbolProvider",
+    editor.document.uri
+  );
+
+  const trail = findTrail(symbols ?? [], position);
+
+  return trail.join(".");
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "vs-code-translation-extension.copyJsonKey",
+    async () => {
+      const jsonPath = await getSymbolTrail();
+
+      await vscode.env.clipboard.writeText(jsonPath);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
